14aabb681f00568fd9109f5380f48554dd4097d7
Arison Firmino <arisonfirmino@hotmail.com>
2023-11-18 22:35:02 -0300

    welcome screen created

---
+++ src/pages/Welcome/index.js
@@ -0,0 +1,41 @@
+import { Text, View, Image, TouchableOpacity } from "react-native";
+
+import { LinearGradient } from "expo-linear-gradient";
+
+export default function Welcome() {
+  return (
+    <View className="bg-screen-color flex-1 px-5 py-14">
+      <View className="items-center justify-center">
+        <Image
+          source={require("../../assets/logo.png")}
+          className="h-[272px] w-[200px]"
+        />
+      </View>
+
+      <View className="flex-1 justify-center">
+        <Text className="text-text-color text-6xl">Gerencie</Text>
+        <Text className="text-text-color text-6xl">Suas</Text>
+        <Text className="text-text-color text-6xl">Finanças</Text>
+
+        <Text className="text-text-color w-64 text-lg opacity-75">
+          Acompanhe todo dinheiro entrando e saindo de suas contas.
+        </Text>
+      </View>
+
+      <View className="flex-row justify-between">
+        <TouchableOpacity className="border-bg-color flex h-14 w-40 items-center justify-center rounded-[15px] border border-solid">
+          <Text className="text-text-color text-xl">Cadastrar</Text>
+        </TouchableOpacity>
+
+        <LinearGradient
+          colors={["#2fd78a", "#239e6f"]}
+          className="rounded-[15px]"
+        >
+          <TouchableOpacity className="flex h-14 w-40 items-center justify-center">
+            <Text className="text-xl text-white">Fazer Login</Text>
+          </TouchableOpacity>
+        </LinearGradient>
+      </View>
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "screen-color": "#e1e1e1",
+        "bg-color": "#2fd78a",
+        "text-color": "#304c5a",
+      },
+    },
+  },
+  plugins: [],
+};
